bd2abf5a49fb15074fd3dc5ee5c1b7a85f7961e0
AbdulkarimMziya <66888226+AbdulkarimMziya@users.noreply.github.com>
2023-12-28 16:47:18 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './app.scss'
+import Calculator from './Components/Calculator/Calculator'
+
+
+function App() {
+  
+
+  return (
+    <div className="app">
+      <Calculator />
+    </div>
+      
+  )
+}
+
+export default App
